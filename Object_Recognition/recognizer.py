4e7e2412f3ec617010b875c41cdf49eece562c3d
AbhayaU <93063950+AbhayaU@users.noreply.github.com>
2022-11-12 21:42:27 -0500

    Code for 10 items
    check real quick

--- Object_Recognition/recognizer.py
+++ Object_Recognition/recognizer.py
@@ -23,5 +23,24 @@ recognition = recognizer.detectObjectsFromImage(
     )  
   
 # iterating through the items found in the image  
-for eachItem in recognition:  
-    print(eachItem["name"] , " : ", eachItem["percentage_probability"])
+#for eachItem in recognition:  
+    #print(eachItem["name"] , " : ", eachItem["percentage_probability"])
+
+#Jakes Carbonemmions conversion
+
+#example 
+
+
+objectList = ['pizza','bicyle', 'cow', 'pig/pork', 't-shirt', 'laptop', 'phone', 'shoes', 'chicken', 'person' ]
+emmisionList = [5.1, 96, 36, 1.8, 6.75, 422.5, 63, 14, 12.27, 14515 ]
+
+def objcetCarbonFootPrint(object, footprint):
+    print(f"{object} has a footprint of {float(footprint)} kgs")
+
+for eachItem in recognition:
+    object = eachItem
+    index = objectList.index(object)
+    footfprint = emmisionList(index)
+    objcetCarbonFootPrint(object, footfprint)
+
+    
